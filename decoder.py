ba742d40e45f551bdead2996ecdd140965d6732f
springbot2025 <2922562941@qq.com>
2026-03-02 19:30:57 +0800

    first commit

---
+++ decoder.py
@@ -0,0 +1,60 @@
+class Decode:
+    def __init__(self, cypher, key = None, flag_header = "flag{"):
+        self.c = cypher
+        self.k = key
+        self.fh = flag_header
+    
+    def clean(self):
+        self.c = "".join(self.c.split()).replace(",","")
+    
+    def base64(self):
+        self.clean()
+        import base64
+        padding = len(self.c) % 4
+        if padding:
+            self.c += "=" * (4 - padding)
+        try:
+            m = base64.b64decode(self.c).decode('utf-8', errors = 'ignore')
+        except Exception as e:
+            print(f"base64解码失败，原因: {e}")
+            return
+        print(f"原始解码结果为\"{m}\"")
+        self._find_flag(m)
+    
+    def hex(self):
+        self.clean()
+        self.c = self.c.lower()
+        self.c = self.c.replace("0x", "")
+        try:
+            m = bytes.fromhex(self.c).decode('utf-8', errors = 'ignore')
+        except Exception as e:
+            print(f"hex解码失败，原因: {e}")
+            return
+        print(f"原始解码结果为\"{m}\"")
+        self._find_flag(m)
+    
+    def binary(self):
+        self.clean()
+        self.c = self.c.replace("0b", "")
+        try:
+            n = int(self.c, 2)
+            m = n.to_bytes((n.bit_length() + 7) // 8, 'big').decode('utf-8', errors = 'ignore')
+        except Exception as e:
+            print(f"binary解码失败，原因: {e}")
+            return
+        print(f"原始解码结果为\"{m}\"")
+        self._find_flag(m)
+    
+    def xor(self):
+        self.clean()
+        if not self.k:
+            print("xor解码需要密钥")
+            return
+        
+    def _find_flag(self, m):
+        if self.fh and self.fh in m:
+                    print("找到flag:")
+                    flag = self.fh + m.split("{")[1].split("}")[0]
+                    print(f"{flag}")
+        else:
+            print("未找到flag, 可能需要多次嵌套解码")
---
+++ main.py
@@ -0,0 +1,48 @@
+'''
+CTF一把梭神秘小工具
+'''
+from decoder import Decode
+import time
+
+def main():
+    while True:
+        print("请选择你需要的功能：")
+        print("1. 解码工具（自动寻找flag）")
+        print("2. 长文本寻找flag")
+        choice = input("请输入编号：")
+        if choice == "1":
+            c = input("请输入密文:\n")
+            flag_header = input("请输入flag的前缀（输入回车则默认为flag{）：")
+            if flag_header.strip() == "":
+                flag_header = "flag{"
+            key = input("请输入密钥：（无密码则输入回车）\n")
+            if key.strip() == "":
+                key = None
+            if not key:
+                choice2 = input("你没有输入密钥，请选择解码方式：1. base64 2. hex 3. binary\n")
+                decoder = Decode(c, key, flag_header = flag_header)
+                if choice2 == "1":
+                    decoder.base64()
+                elif choice2 == "2":
+                    decoder.hex()
+                elif choice2 == "3":
+                    decoder.binary()
+        if choice == "2":
+            text = input("请输入文本:\n")
+            flag_header = input("请输入flag的前缀（输入回车则默认为flag）：")
+            if flag_header.strip() == "":
+                flag_header = "flag{"
+            decoder = Decode("", flag_header = flag_header)
+            decoder._find_flag(text)
+        else:
+            print("请输入正确的编号！")
+        i = input("输入quit退出，输入回车继续：")
+        if i == "quit":
+            print("感谢使用，再见！")
+            time.sleep(2)
+            break
+
+if __name__ == '__main__':
+    print("=" * 30)
+    print("欢迎使用Spring的CTF工具！")
+    main()
